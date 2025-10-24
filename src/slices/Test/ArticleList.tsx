1666cbba5216a803899d279f822b9b79f0091946
Samuel Horn af Rantzien <samuel@samuelhorn.com>
2025-10-24 13:45:53 +0200

    refactor: fixed issues

--- src/slices/Test/ArticleList.tsx
+++ src/slices/Test/ArticleList.tsx
@@ -1,43 +1,79 @@
-"use client"
+"use client";
 
-import React, { useState } from 'react'
-import { ArticleDocument, PlatformsDocument } from '../../../prismicio-types';
-import { asText, Content } from '@prismicio/client';
+import React, { useState } from "react";
+import {
+  ArticleDocument,
+  ArticleTypesDocument,
+  PlatformsDocument,
+} from "../../../prismicio-types";
+import { asText, Content, isFilled } from "@prismicio/client";
+import { PrismicNextLink } from "@prismicio/next";
 
-export default function ArticleList({ articles, platforms, articleTypes, slice }: { 
-    articles: ArticleDocument[]; 
-    platforms: PlatformsDocument[]; 
-    articleTypes: ArticleDocument[]; 
-    slice: Content.TestSlice 
+export default function ArticleList({
+  articles,
+  platforms,
+  articleTypes,
+  slice,
+}: {
+  articles: ArticleDocument[];
+  platforms: PlatformsDocument[];
+  articleTypes: ArticleTypesDocument[];
+  slice: Content.TestSlice;
 }) {
+  const [activePlatform, setActivePlatform] = useState<string>("web");
 
-    const [activePlatform, setActivePlatform] = useState<string>("web");
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <div style={{ display: "flex", flexDirection: "row", gap: "1rem" }}>
+        {platforms.map((platform) => (
+          <button
+            style={{
+              padding: "1rem",
+              border: "2px solid #fff",
+              backgroundColor:
+                activePlatform === platform.uid ? "#63656A" : "#36373A",
+              cursor: "pointer",
+            }}
+            key={platform.id}
+            onClick={() => setActivePlatform(platform.uid)}
+          >
+            {asText(platform.data.title)}
+          </button>
+        ))}
+      </div>
+      <div style={{ display: "flex", gap: "1rem" }}>
+        {articleTypes.map((articleType) => (
+          <div
+            key={articleType.id}
+            style={{ border: "2px solid #fff", padding: "1rem" }}
+          >
+            <h2>{asText(articleType.data.title)}</h2>
+            <ul>
+              {articles.map((article) => {
+                if (
+                  !isFilled.contentRelationship(article.data.article_type) ||
+                  !isFilled.contentRelationship(article.data.platform) ||
+                  article.data.platform.uid !== activePlatform ||
+                  article.data.article_type.uid !== articleType.uid
+                ) {
+                  return null;
+                }
 
-
-
-    return (
-        <section
-            data-slice-type={slice.slice_type}
-            data-slice-variation={slice.variation}
-        >
-            {articleTypes.map((articleType) => (
-                <div>
-                    <h2>
-                        {asText(articleType.data.title)}
-                    </h2>
-                    <ul>
-                        {articles.map((article) => {
-
-                            const articleType = article.data.article_type.data.title
-                            console.log(articleType)
-
-                            return (
-                                <li key={article.id}></li>
-                            )
-                        })}
-                    </ul>
-                </div>
-            ))}
-        </section>
-    )
+                return (
+                  <li key={article.id}>
+                    <PrismicNextLink href={`/articles/${article.uid}`}>
+                      {asText(article.data.title)}
+                    </PrismicNextLink>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
