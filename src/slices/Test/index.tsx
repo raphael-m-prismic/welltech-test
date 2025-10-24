f94cc93e3053d235c8a1debcb5690368531620d9
Samuel Horn af Rantzien <samuel@samuelhorn.com>
2025-10-24 13:45:08 +0200

    refactor: removed unused imports

--- src/slices/Test/index.tsx
+++ src/slices/Test/index.tsx
@@ -1,25 +1,8 @@
-
-
 import { FC, JSX } from "react";
 import { Content } from "@prismicio/client";
 import { createClient } from "@/prismicio";
-import { PrismicRichText, PrismicText, SliceComponentProps } from "@prismicio/react";
-
-import Container from "@/components/Container";
-
-import styles from "./index.module.css";
-
-import Web from "@/components/icons/Web";
-import Android from "@/components/icons/Android";
-import Ios from "@/components/icons/Ios";
-import { PrismicNextLink } from "@prismicio/next";
+import { SliceComponentProps } from "@prismicio/react";
 import ArticleList from "./ArticleList";
-
-const platformIcons: Record<string, JSX.Element> = {
-  web: <Web />,
-  android: <Android />,
-  ios: <Ios />,
-};
 
 export type TestProps = SliceComponentProps<Content.TestSlice> & {
   context?: {
@@ -27,18 +10,19 @@ export type TestProps = SliceComponentProps<Content.TestSlice> & {
   };
 };
 
-const platforms = ["web", "android", "ios"];
-const articleTypes = ["first-steps", "tutorials", "other"];
-
-const Test: FC<TestProps> =  async ({ slice }) => {
+const Test: FC<TestProps> = async ({ slice }) => {
   const client = createClient();
   const allArticles = await client.getAllByType("article");
   const allPlatforms = await client.getAllByType("platforms");
   const allTypes = await client.getAllByType("article_types");
 
-
   return (
-    <ArticleList articles={allArticles} platforms={allPlatforms}  articleTypes={allTypes} slice={slice}/>
+    <ArticleList
+      articles={allArticles}
+      platforms={allPlatforms}
+      articleTypes={allTypes}
+      slice={slice}
+    />
   );
 };
 
